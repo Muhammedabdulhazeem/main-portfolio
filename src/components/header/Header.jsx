3984c412f00d49452f71309603ec8e7c88490f81
Abdulhazeem <abdulhazeemmuhammed@gmail.com>
2022-10-06 09:04:08 +0100

    modified

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -61,14 +61,16 @@ const Header = () => {
                 className="animate-logo-2"
               />
             </svg> */}
-            <svg class="w-6 h-6" 
+            <svg class="home-logo" 
               fill="currentColor" 
               viewBox="0 0 20 20" 
               // className="home-logo"
-              style={{width: "90px", height: "90px", marginBottom: "-10px"}}
+              // style={{width: "90px", height: "90px", marginBottom: "-10px"}}
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 
+              <path 
+                className="lower-logo-color fill__color home-logo animate-logo-1"
+                d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 
                 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 
                 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z">
               </path>
